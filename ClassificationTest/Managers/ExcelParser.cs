dddb292df0679a93305bd5274aa022d448ca419c
Ondrej Tadanai <tadanaio@gmail.com>
2017-09-26 15:01:27 +0200

    first commit

---
+++ ClassificationTest/Managers/ExcelParser.cs
@@ -0,0 +1,55 @@
+﻿using ClassificationTest.DTO;
+using OfficeOpenXml;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+//using TemperatureDataVW.DTO;
+
+namespace ClassificationTest.Managers
+{
+    class ExcelParser
+    {
+        string _fileName;
+        double[][] input;
+        double[][] output;
+
+        //List<VwTempData> dataList = new List<VwTempData>();
+
+
+        public ExcelParser(string fileName)
+        {
+            if (string.IsNullOrWhiteSpace(fileName))
+            {
+                throw new ArgumentException("param cannot be empty string", nameof(fileName));
+            }
+
+            _fileName = fileName;
+        }
+
+        public void ParseSourceData()
+        {
+            var i = 1;
+            var cnt = 0;
+            using (ExcelPackage sourceFile = new ExcelPackage(new FileInfo(_fileName)))
+            {
+                var myWorksheet = sourceFile.Workbook.Worksheets.First(); //select sheet here
+                var totalRows = myWorksheet.Dimension.End.Row;
+                var totalColumns = myWorksheet.Dimension.End.Column;
+                cnt = totalRows;
+
+                var sb = new StringBuilder(); //this is your your data
+                for (int rowNum = 2; rowNum <= totalRows; rowNum++) //select starting row here
+                {
+                    var row = myWorksheet.Cells[rowNum, 1, rowNum, totalColumns];
+
+                    NeuronData.AddExcelRowData(row);
+                    Console.WriteLine($"parsing: - {i}/{cnt}");
+                    i++;
+                }
+            }
+        }
+    }
+}
--- ClassificationTest/Program.cs
+++ ClassificationTest/Program.cs
@@ -1,15 +1,78 @@
-﻿using System;
+﻿using AForge.Neuro;
+using AForge.Neuro.Learning;
+using ClassificationTest.DTO;
+using ClassificationTest.Managers;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Forms;
 
 namespace ClassificationTest
 {
     class Program
     {
+        [STAThread]
         static void Main(string[] args)
         {
+            //vybrat excel s hodnotami
+            //vyparsovat ho, ulozit si hodnoty
+
+            //rozbehat neuronku
+            //naucit neuronku
+            //testovat neuronku
+            //skusit pustit data on-the-fly
+
+            OpenFileDialog ofd = new OpenFileDialog()
+            {
+                Multiselect = false
+            };
+
+            string excelFileName;
+
+            if (ofd.ShowDialog() == DialogResult.OK)
+            {
+                excelFileName = ofd.FileName;
+            }
+            else
+            {
+                throw new Exception("Je nutne vybrat subor");
+            }
+
+            var excelManager = new ExcelParser(excelFileName);
+            excelManager.ParseSourceData();
+            
+
+            double[][] input = NeuronData.GetInputMatrix();
+            double[][] output = NeuronData.GetOutputMatrix();
+
+            // create neural network
+            ActivationNetwork network = new ActivationNetwork(
+                new SigmoidFunction(3),
+                3, // two inputs in the network
+                3, // two neurons in the first layer
+                2); // one neuron in the second layer
+                    // create teacher
+            BackPropagationLearning teacher =
+                new BackPropagationLearning(network);
+            bool needToStop = false;
+            // loop
+            while (!needToStop)
+            {
+                // run epoch of learning procedure
+                System.Threading.Thread.Sleep(1000);
+                double error = teacher.RunEpoch(input, output);
+                Console.WriteLine(error);
+                // check error value to see if we need to stop
+                // ...
+            }
+
+
+
+
+
+
         }
     }
 }
